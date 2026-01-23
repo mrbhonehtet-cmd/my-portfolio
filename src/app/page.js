6eca32b01d4b4778129f7482025acece53b65fb6
mrbhonehtet-cmd <mr.bhonehtet@gmail.com>
2026-01-23 12:54:23 +0630

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,9 @@ export default function Home() {
       <main>
         <Hero />
         <About />
-        <Skills />
-        <Projects />
         <ImageGallery />
+        <Skills />
+        <Projects />       
         <Contact />
       </main>
       <Footer />
